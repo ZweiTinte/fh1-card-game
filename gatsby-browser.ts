74aaf18b41f69a58d547c4adbc162b9395b6a207
ZweiTinte <zweitinte@gmail.com>
2022-11-19 14:24:52 +0100

    clean up

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,6 +1,7 @@
 import "./src/styles/main.scss";
 import "./src/styles/buttons.scss";
 import "./src/styles/forms.scss";
+import "./src/styles/headlines.scss";
 
 import WrapRootElement from "./src/wrapRootElement";
 
